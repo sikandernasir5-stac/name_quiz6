675f32e0f9e0d18a63082c2f6e9dca9dceb85390
sikandernasir5-stac <sikandernasir5@gmail.com>
2026-09-15 16:18:16 +0500

    nn

--- src/app/page.js
+++ src/app/page.js
@@ -3,15 +3,19 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (
-  <mein styles={{color/:'black'}}>
-  <div styles={{display:'flex',flexdirecton:'coloumn'}}                                               >
-  {booking.map((b)=>(
-   <div key={b.id}>
-   <Image src >  
-
-
-
-
+    <main style={{ color: 'black' }}>
+      <div style={{ display: 'flex', flexDirection: 'column' }}>
+        {booking.map((b) => (
+          <div key={b.id}>
+            <Image 
+              src={b.imageUrl} 
+              alt={b.title || "Booking image"} 
+              width={500} 
+              height={300} 
+            />
+          </div>
+        ))}
+      </div>
+    </main>
   );
 }
-
