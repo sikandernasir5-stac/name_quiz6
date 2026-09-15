4c65108bea360a56e48303f05b505ff3ea12ea08
sikandernasir5-stac <sikandernasir5@gmail.com>
2026-09-15 14:16:25 +0500

    siaknder

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
+  images:{
+    remotePatterns:[
+      {hostname:"res.cloudinary.com"}
+    ]
+  }
 };
 
 export default nextConfig;
